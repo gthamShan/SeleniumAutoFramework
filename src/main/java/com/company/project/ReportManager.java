2c952ca70d825b63f197e1b9638a8083068b84fa
gthamShan <96945141+gthamShan@users.noreply.github.com>
2025-04-19 12:15:34 -0500

    added log method in ReportManager class

--- src/main/java/com/company/project/ReportManager.java
+++ src/main/java/com/company/project/ReportManager.java
@@ -1,11 +1,13 @@
 package com.company.project;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ReportManager {
 	private static ExtentReports extent;
+	private static ExtentTest test;
 
 	public static void setupReport() {
 
@@ -13,8 +15,12 @@ public class ReportManager {
 		ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
 		extent.attachReporter(spark);
 
-		extent.createTest("MyfirstTest").log(Status.PASS, "This is a logging event");
+		test = extent.createTest("MyfirstTest").log(Status.PASS, "This is a logging event");
 		extent.flush();
+	}
+
+	public static void log(String message) {
+		test.info(message);
 	}
 
 }
